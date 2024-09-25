c1e130ee07dd311c73eae0fa395e69eb188f1f69
loki.zuo <zyhttx@Gmail.com>
2024-09-25 10:43:35 +0800

    init

---
+++ config/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
+from config.env import DataBaseConfig
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DataBaseConfig.db_username}:{quote_plus(DataBaseConfig.db_password)}@" \
+                          f"{DataBaseConfig.db_host}:{DataBaseConfig.db_port}/{DataBaseConfig.db_database}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, echo=DataBaseConfig.db_echo,
+    pool_size=700, max_overflow=1400,
+    pool_pre_ping=True
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
